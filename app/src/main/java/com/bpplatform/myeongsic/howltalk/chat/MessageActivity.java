29676fbe6afb7059a149736132b0245a5384baaf
MyeongSic <you6878@icloud.com>
2017-10-09 17:00:29 +0900

    init

---
+++ app/src/main/java/com/bpplatform/myeongsic/howltalk/chat/MessageActivity.java
@@ -0,0 +1,163 @@
+package com.bpplatform.myeongsic.howltalk.chat;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.bpplatform.myeongsic.howltalk.R;
+import com.bpplatform.myeongsic.howltalk.model.ChatModel;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MessageActivity extends AppCompatActivity {
+
+    private String destinatonUid;
+    private Button button;
+    private EditText editText;
+
+    private String uid;
+    private String chatRoomUid;
+
+    private RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_message2);
+        uid = FirebaseAuth.getInstance().getCurrentUser().getUid();  //채팅을 요구 하는 아아디 즉 단말기에 로그인된 UID
+        destinatonUid = getIntent().getStringExtra("destinationUid"); // 채팅을 당하는 아이디
+        button = (Button) findViewById(R.id.messageActivity_button);
+        editText = (EditText) findViewById(R.id.messageActivity_editText);
+
+        recyclerView = (RecyclerView)findViewById(R.id.messageActivity_reclclerview);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ChatModel chatModel = new ChatModel();
+                chatModel.users.put(uid,true);
+                chatModel.users.put(destinatonUid,true);
+
+
+
+
+                if(chatRoomUid == null){
+                    button.setEnabled(false);
+                    FirebaseDatabase.getInstance().getReference().child("chatrooms").push().setValue(chatModel).addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            checkChatRoom();
+                        }
+                    });
+
+                }else {
+
+                    ChatModel.Comment comment = new ChatModel.Comment();
+                    comment.uid = uid;
+                    comment.message = editText.getText().toString();
+                    FirebaseDatabase.getInstance().getReference().child("chatrooms").child(chatRoomUid).child("comments").push().setValue(comment);
+                }
+
+
+            }
+        });
+        checkChatRoom();
+
+
+    }
+
+    void  checkChatRoom(){
+
+        FirebaseDatabase.getInstance().getReference().child("chatrooms").orderByChild("users/"+uid).equalTo(true).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for(DataSnapshot item : dataSnapshot.getChildren()){
+                    ChatModel  chatModel = item.getValue(ChatModel.class);
+                        if(chatModel.users.containsKey(destinatonUid)){
+                            chatRoomUid = item.getKey();
+                            button.setEnabled(true);
+                            recyclerView.setLayoutManager(new LinearLayoutManager(MessageActivity.this));
+                            recyclerView.setAdapter(new RecyclerViewAdapter());
+                        }
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+    class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
+
+
+        List<ChatModel.Comment> comments;
+        public RecyclerViewAdapter() {
+            comments = new ArrayList<>();
+
+            FirebaseDatabase.getInstance().getReference().child("chatrooms").child(chatRoomUid).child("comments").addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    comments.clear();
+
+                    for(DataSnapshot item : dataSnapshot.getChildren()){
+                        comments.add(item.getValue(ChatModel.Comment.class));
+                    }
+
+                    notifyDataSetChanged();
+
+
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+
+
+        }
+
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_message,parent,false);
+
+
+            return new MessageViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+
+            ((MessageViewHolder)holder).textView_message.setText(comments.get(position).message);
+        }
+
+        @Override
+        public int getItemCount() {
+            return comments.size();
+        }
+
+        private class MessageViewHolder extends RecyclerView.ViewHolder {
+            public TextView textView_message;
+
+            public MessageViewHolder(View view) {
+                super(view);
+                textView_message = (TextView) view.findViewById(R.id.messageItem_textView_message);
+            }
+        }
+    }
+}
